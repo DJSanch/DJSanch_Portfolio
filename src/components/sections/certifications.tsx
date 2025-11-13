1bdb3c4f40c5e569d0ffcd38e623a90721a5f30a
DJSanch <160377425+DJSanch@users.noreply.github.com>
2025-11-13 21:14:47 +0800

    Update certifications.tsx

--- src/components/sections/certifications.tsx
+++ src/components/sections/certifications.tsx
@@ -48,7 +48,7 @@ const Certifications = () => {
             Coursera and LinkedIn Learning certifications earned and verified.
           </p>
         </div>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8">
           {[...courseraCerts, ...linkedinCerts].map((cert, index) => {
             let imageBlock = null;
             if (cert.image) {
